dbfbf01828c0b3e729f22d981581f05ef62584aa
김민수 <minsu_510@naver.com>
2025-05-14 15:52:52 +0900

    rename: 0_template 폴더명을 08_template로 변경

---
+++ 08_template/src/components/DeptDetail.vue
@@ -0,0 +1,55 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="container">
+    <div class="row mt-3">
+      <h2 class="bg-info text-light text-center">부서 조회</h2>
+    </div>
+    <form method="post" class="row">
+      <table class="table">
+        <tbody>
+          <tr>
+            <th><label for="deptno">부서번호</label></th>
+            <td>
+              <input
+                type="text"
+                name="deptno"
+                id="deptno"
+                v-model.lazy="dept.deptno"
+                :readonly="dept != null && dept.deptno"
+              />
+            </td>
+          </tr>
+          <tr>
+            <th><label for="dname">부서이름</label></th>
+            <td>
+              <input type="text" name="dname" id="dname" v-model="dept.dname" />
+            </td>
+          </tr>
+          <tr>
+            <th><label for="loc">지역</label></th>
+            <td>
+              <input type="text" name="loc" id="loc" v-model="dept.loc" />
+            </td>
+          </tr>
+        </tbody>
+        <tfoot>
+          <tr>
+            <td colspan="2">
+              <input type="button" value="수정" class="btn btn-warning m-1"/>
+              <input
+                type="button"
+                value="삭제"
+                class="btn btn-dark m-1"
+              />
+            </td>
+          </tr>
+        </tfoot>
+      </table>
+    </form>
+  </div>
+</template>
+
+<style scoped></style>
