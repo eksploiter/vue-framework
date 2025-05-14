b343204524ac57d67bed3a199c69e974b0971009
김민수 <minsu_510@naver.com>
2025-05-14 21:06:19 +0900

    feat: DeptDetail.vue에 선택한 부서 정보 출력 기능 추가 (#1)

--- 08_template/src/components/DeptDetail.vue
+++ 08_template/src/components/DeptDetail.vue
@@ -1,6 +1,37 @@
 <script setup>
+import { reactive, watch } from 'vue'
+const props = defineProps({
+  dept: {
+    tupe: Object,
+    required: true,
+  },
+})
+const emit = defineEmits(['updateDept', 'deleteDept'])
 
+const localDept = reactive({
+  deptno: '',
+  dname: '',
+  loc: '',
+})
+
+// props.dept 값이 바뀔 때마다 localDept에 복사
+watch(
+  () => props.dept,
+  (newVal) => {
+    console.log('watch fired', newVal) // 디버깅
+    if (newVal) {
+      localDept.deptno = newVal.deptno
+      localDept.dname = newVal.dname
+      localDept.loc = newVal.loc
+    }
+  },
+  { immediate: true },
+)
+
+const update = () => emit('updateDept', { ...localDept })
+const remove = () => emit('deleteDept', localDept.deptno)
 </script>
+<!--반드시 로컬 복사본(localDept) 만들어서 바인딩해야 함-->
 
 <template>
   <div class="container">
@@ -17,7 +48,7 @@
                 type="text"
                 name="deptno"
                 id="deptno"
-                v-model.lazy="dept.deptno"
+                v-model.lazy="localDept.deptno"
                 :readonly="dept != null && dept.deptno"
               />
             </td>
@@ -25,25 +56,21 @@
           <tr>
             <th><label for="dname">부서이름</label></th>
             <td>
-              <input type="text" name="dname" id="dname" v-model="dept.dname" />
+              <input type="text" name="dname" id="dname" v-model="localDept.dname" />
             </td>
           </tr>
           <tr>
             <th><label for="loc">지역</label></th>
             <td>
-              <input type="text" name="loc" id="loc" v-model="dept.loc" />
+              <input type="text" name="loc" id="loc" v-model="localDept.loc" />
             </td>
           </tr>
         </tbody>
         <tfoot>
           <tr>
             <td colspan="2">
-              <input type="button" value="수정" class="btn btn-warning m-1"/>
-              <input
-                type="button"
-                value="삭제"
-                class="btn btn-dark m-1"
-              />
+              <input type="button" value="수정" class="btn btn-warning m-1" @click="update" />
+              <input type="button" value="삭제" class="btn btn-dark m-1" @click="remove" />
             </td>
           </tr>
         </tfoot>
